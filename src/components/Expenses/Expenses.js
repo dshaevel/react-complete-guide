f5e62c79bcb1bddf34853518d7a920cb703cfb54
David Shaevel <david@davidshaevel.com>
2021-05-19 21:23:16 -0500

    filteredExpenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,10 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className='expenses'>
@@ -19,7 +23,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => {
+        {filteredExpenses.map((expense) => {
           return (
             <ExpenseItem
               key={expense.id}
