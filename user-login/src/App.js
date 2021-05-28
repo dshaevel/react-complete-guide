43517bfade8d31eb624464662830d2cb3234c63a
David Shaevel <david@davidshaevel.com>
2021-05-28 18:43:11 -0500

    intro to context

--- user-login/src/App.js
+++ user-login/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
+import AuthContext from './store/auth-context';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
@@ -28,13 +29,13 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
